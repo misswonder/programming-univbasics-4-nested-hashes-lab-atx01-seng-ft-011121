87219f4d05f5faff535a1d6a310db55e93ac6833
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-22 18:58:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = 
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -127,5 +128,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
